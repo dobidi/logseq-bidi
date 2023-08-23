a76ce52121a9f6481e24d5767fd9fc4276fe5554
Mostafa Ahangarha <ahangarha@riseup.net>
2023-08-24 00:00:06 +0330

    Prepare for first release (#1)
    * Add readme
    * Add icon and license file
    * Use parcel for bundling
    * Add CI for logseq marketplace

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+import '@logseq/libs'
+
 const graphDocument = window.parent.document
 
 const setDirAuto = (node) => node.setAttribute('dir', 'auto')
 const setDirAutoToAll = (nodes) => nodes.forEach((node) => setDirAuto(node))
-
-
 
 const applyBidi = () => {
   const cssSelector = 'div.ls-block:not([dir]), div.ls-page-title:not([dir])'
@@ -21,7 +21,7 @@ const applyBidi = () => {
         console.log(addedNode)
         if (addedNode.classList?.contains('ls-block')) setDirAuto(addedNode)
         
-        subLsBlocks = addedNode.querySelectorAll(cssSelector)
+        const subLsBlocks = addedNode.querySelectorAll(cssSelector)
         setDirAutoToAll(subLsBlocks)
       }
     }
